3b54012b7e43cddb1a598eac65852a85f28b6bad
slavon360 <slavon360@github.com>
2022-10-23 19:49:33 +0300

    products-catalog (list-view WIP)

--- vts/catalog/views.py
+++ vts/catalog/views.py
@@ -27,32 +27,21 @@ def index(request):
 
 	return render(request, 'index.html', context=context)
 
-class ProductDetailView(DetailView):
-	model = Product
-
-	def get_context_data(self, **kwargs):
-		context = super().get_context_data(**kwargs)
-		context['categories'] = Category.objects.all()
-		context['phones'] = Phone.objects.all()
-		return context
-
-class ProductsCatalogView(ListView):
-	model = Product
-	paginate_by = 12
-	orphans = 4
-	template_name = 'catalog/products_catalog.html'
-	context_object_name = 'products_catalog'
-
-	def get_breadcrumbs(*args, **kwargs):
+class Breadcrumbs():
+	def get_breadcrumbs(self, *args, **kwargs):
 		breadcrumbs = []
 		def add_category_breadcrumb(categ):
 			if str(categ.id) == str(kwargs['category']):
+				if hasattr(self, 'page_title'):	
+					self.page_title += categ.name
 				breadcrumbs.append({ 'title': categ.name, 'link': f'products-catalog/{categ.id}' })
 
-		def add_subcategy_breadcrumb(categ):
+		def add_subcategory_breadcrumb(categ):
 			if str(categ.id) == str(kwargs['category']):
 				for subcateg in categ.subcategory_set.all():
 					if str(subcateg.id) == str(kwargs['subcategory']):
+						if hasattr(self, 'page_title'):
+							self.page_title += subcateg.name
 						breadcrumbs.append({
 							'title': subcateg.name,
 							'link': '',
@@ -63,18 +52,22 @@ class ProductsCatalogView(ListView):
 				if str(subsubcateg.id) == str(kwargs['subsubcategory']):
 					already_exist = next((item for item in breadcrumbs if str(item['id']) == str(kwargs['subsubcategory'])), None)
 					if not already_exist:
+						if hasattr(self, 'page_title'):
+							self.page_title += f' {subsubcateg.name}'
 						breadcrumbs.append({
 							'title': subsubcateg.name,
 							'id': subsubcateg.id,
 							'link': ''
 						})
 
-		def add_subsubcategy_and_subcategy_breadcrumb(categ):
+		def add_subsubcategory_and_subcategory_breadcrumb(categ):
 			for subcateg in categ.subcategory_set.all():
 				if str(subcateg.id) == str(kwargs['subcategory']):
+					if hasattr(self, 'page_title'):
+						self.page_title += subcateg.name
 					breadcrumbs.append({
 						'title': subcateg.name,
-						'link': f'products-subcatalog/{subcateg.id}',
+						'link': f'products-subcatalog/{categ.id}/{subcateg.id}',
 						'id': subcateg.id
 					})
 					add_subsubcategory_breadcrumb(subcateg)
@@ -83,13 +76,44 @@ class ProductsCatalogView(ListView):
 			if kwargs['category'] and not kwargs['subcategory']:
 				add_category_breadcrumb(categ)
 			elif kwargs['subcategory'] and kwargs['subsubcategory']:
-				add_subsubcategy_and_subcategy_breadcrumb(categ)
+				add_subsubcategory_and_subcategory_breadcrumb(categ)
 			elif kwargs['category'] and kwargs['subcategory']:
 				add_category_breadcrumb(categ)
-				add_subcategy_breadcrumb(categ)
+				add_subcategory_breadcrumb(categ)
 
 		sorted_breadcrumbs = sorted(breadcrumbs, key=itemgetter('link'), reverse=True)
 		return sorted_breadcrumbs
+
+class ProductDetailView(DetailView, Breadcrumbs):
+	model = Product
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		categ = self.object.category.all()[0]
+		subcateg = self.object.subcategory.all()[0]
+		subsubcateg = self.object.subsubcategory.all()[0]
+		category_id = getattr(categ, 'id')
+		subcategory_id = getattr(subcateg, 'id')
+		subsubcategory_id = getattr(subsubcateg, 'id')
+
+		context['categories'] = Category.objects.all()
+		context['phones'] = Phone.objects.all()
+		context['breadcrumbs'] = self.get_breadcrumbs(
+			self,
+			None,
+			category = category_id,
+			subcategory = subcategory_id,
+			subsubcategory = subsubcategory_id
+		)
+		return context
+
+class ProductsCatalogView(ListView, Breadcrumbs):
+	model = Product
+	paginate_by = 12
+	orphans = 4
+	template_name = 'catalog/products_catalog.html'
+	context_object_name = 'products_catalog'
+	page_title = 'VTS | '
 
 	def get_queryset(self):
 		queryset = super().get_queryset()
@@ -110,9 +134,11 @@ class ProductsCatalogView(ListView):
 		context['categories'] = Category.objects.all()
 		context['phones'] = Phone.objects.all()
 		context['breadcrumbs'] = self.get_breadcrumbs(
+			self,
 			None,
 			category = self.kwargs['category'],
 			subcategory = self.kwargs['subcategory'],
 			subsubcategory = self.kwargs['subsubcategory']
 		)
+		context['page_title'] = self.page_title
 		return context
