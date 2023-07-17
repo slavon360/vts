d00aafa0585456404f085ac6299bb935284f6544
viacheslav <slavon360@gmail.com>
2023-07-18 00:33:26 +0300

    screen width

--- frontend/js/utils/store-device-width.js
+++ frontend/js/utils/store-device-width.js
@@ -1,8 +1,9 @@
 const storeDeviceWidth = () => {
     window.addEventListener('DOMContentLoaded', () => {
-        const device_width = window.innerWidth || document.documentElement.clientWidth;
+        const device_width = window.innerWidth || document.documentElement.clientWidth || screen && screen.width;
 
         document.cookie = `device-width=${device_width}`;
+        console.log('device_width: ', device_width);
     });
 };
 
