5322694d7e7de43e135893485dd0126e8b50f5b1
viacheslav <slavon360@gmail.com>
2023-11-26 13:49:49 +0200

    fix

--- backend/vts/settings.py
+++ backend/vts/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = path.join(BASE_DIR, 'static/')
+STATIC_ROOT = '/static'
 
 MEDIA_ROOT =  path.join(BASE_DIR, 'image')
 MEDIA_URL = 'image/'
