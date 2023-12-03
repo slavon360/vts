037902c4d155bac93ae08ca5f74ba8288bc2cb28
viacheslav <slavon360@gmail.com>
2023-12-03 18:44:19 +0200

    try to fix bug with static/admin files

--- backend/vts/settings.py
+++ backend/vts/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static'
+STATIC_ROOT = path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT =  path.join(BASE_DIR, 'image')
 MEDIA_URL = 'image/'
